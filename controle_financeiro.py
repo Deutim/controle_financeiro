070bccdd23933b98cc70b28b7fbfd8039b6574e7
Deutim <adeiltoj93@gmail.com>
2026-03-26 23:37:21 -0300

    Adiciona controle_financeiro.py e arquivos de mapeamento de dados

---
+++ controle_financeiro.py
@@ -0,0 +1,16 @@
+
+print("""
+    ========== CONTROLE FINANCEIRO ==========
+      
+    1 - Cadastrar
+    2 - Listar
+    3 - Saldo
+    0 - Sair
+      
+    =========================================
+
+""")
+
+
+
+
